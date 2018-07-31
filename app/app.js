a18b6aade5d9b6f8b770c22cfe18edd544dccbb7
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-07-31 10:56:48 -0700

    speaker route working

--- app/app.js
+++ app/app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000 );
 
+// send info from main file to speakers.js
+app.set('appData', dataFile); // global variable appData will now hold the dataFile
+
 app.use(require('./routes/index'));
+app.use(require('./routes/speakers'));
 
 var server = app.listen(app.get('port'), function(res, req) {
     console.log('Listening on port ' + app.get('port'));
--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -1,4 +1,27 @@
 var express = require('express');
 var router = express();
 
+router.get('/speakers', function(req, res) {
+    var info = "";
+    // get appData from app.js
+    var dataFile = req.app.get('appData');
+
+    // get each speaker info in info variable
+    dataFile.speakers.forEach(function (item) {
+        // `` --> ES6 supports back ticks for multi line
+        info += `
+        <li> 
+            <h2> ${item.name} </h2>
+            <p> ${item.summary} </p>
+        </li>
+        `;
+    });
+
+    // send info variable to the speakers route
+    res.send(`
+        <h1> Roux Meetups </h1>
+        ${info}
+    `);
+});
+
 module.exports = router;
