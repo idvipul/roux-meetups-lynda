3a09c2c21a7d036fbbb7617567a081cce6ac8b2e
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-08-01 12:55:32 -0700

    speakers fixed

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    var data = req.app.get('appData');
+router.get('/', function(req, res) {
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+  var pageSpeakers = data.speakers;
 
-    // stores artworks of every artist
-    var pagePhotos = [];
+  data.speakers.forEach(function(item) {
+    pagePhotos = pagePhotos.concat(item.artwork);
+  });
 
-    data.speakers.forEach(function (item) {
-        // get artworks of eery artist
-        pagePhotos = pagePhotos.concat(item.artwork);
-    });
-
-    res.render('index', {
-        pageTitle: 'Home',
-        artwork: pagePhotos,
-        pageID: 'home'
-    });
+  res.render('index', {
+    pageTitle: 'Home',
+    artwork: pagePhotos,
+    speakers: pageSpeakers,
+    pageID: 'home'
+  });
 
 });
 
