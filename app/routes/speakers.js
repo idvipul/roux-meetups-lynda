7d71fb88330debe17e54f6950284f19d6cbc6156
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-08-01 12:22:53 -0700

    working with speakers page

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -2,23 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/speakers', function(req, res) {
-  var info = '';
-  var dataFile = req.app.get('appData');
-  dataFile.speakers.forEach(function(item) {
-    info += `
-    <li>
-      <h2>${item.name}</h2>
-      <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker">
-      <p>${item.summary}</p>
-    </li>
-    `;
-  });
-  res.send(`
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-      <h1>Roux Academy Meetups</h1>
-      ${info}
-      <script src="/reload/reload.js"></script>
-  `);
+    var data = req.app.get('appData');
+
+    // stores artworks of every artist
+    var pagePhotos = [];
+    var pageSpeakers = data.speakers;
+
+    data.speakers.forEach(function (item) {
+        // get artworks of eery artist
+        pagePhotos = pagePhotos.concat(item.artwork);
+    });
+
+    res.render('speakers', {
+        pageTitle: 'Speakers',
+        artwork: pagePhotos,
+        speakers: pageSpeakers,
+        pageID: 'speakers'
+    });
 });
 
 router.get('/speakers/:speakerid', function(req, res) {
