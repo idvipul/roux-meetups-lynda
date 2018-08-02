7774110c7fa32768a7b5b1740fd9e2f04201ed8c
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-08-01 19:51:52 -0700

    feedback form working

--- app/routes/feedback.js
+++ app/routes/feedback.js
@@ -3,10 +3,11 @@ var router = express.Router();
 
 router.get('/feedback', function(req, res) {
 
-    res.render('feedback', {
-        pageTitle: 'Feedback',
-        pageID: 'feedback'
-    });
+  res.render('feedback', {
+    pageTitle: 'Feedback',
+    pageID: 'feedback'
+  });
+
 });
 
 module.exports = router;
