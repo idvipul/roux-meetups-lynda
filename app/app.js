c3acdc5031a4566e58dc0d9232692490ab0f5028
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-08-02 11:06:53 -0700

    chats working

--- app/app.js
+++ app/app.js
@@ -24,13 +24,10 @@ var server = app.listen(app.get('port'), function() {
 });
 
 io.attach(server);
-
-io.on('connection', function (socket) {
-   console.log('User Connected!');
-
-   socket.on('disconnet', function () {
-     console.log('User Disconnected!')
-   });
+io.on('connection', function(socket) {
+  socket.on('postMessage', function(data) {
+    io.emit('updateMessages', data);
+  });
 });
 
 reload(server, app);
--- app/routes/chat.js
+++ app/routes/chat.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 router.get('/chat', function(req, res) {
 
-    res.render('chat', {
-        pageTitle: 'Chat',
-        pageID: 'chat'
-    });
+  res.render('chat', {
+    pageTitle: 'Chat',
+    pageID: 'chat'
+  });
 
 });
 
